57df22c803268b5c9d5d1c4dee78fd098c4e1b8a
Deivi <davidrgez@gmail.com>
2011-08-27 14:08:28 +0300

    Added improvements developed by Steinkel. Now you can duplicate the button in a view, changing the div id and we can also put the name you want the button in the variable $label

--- views/helpers/upload.php
+++ views/helpers/upload.php
@@ -5,31 +5,56 @@ class UploadHelper extends AppHelper {
 
 	/**
 	 * Shows upload button, including necesary scripts
+	 * 
+	 * @ $label   string, label of button
+	 * @ $group   array with all types of variable, group of variable
+	 * @ $div_id  string, div id to duplicate the buttons
+	 * @ $folder  string
 	 */
-	function button($label, $group = null, $folder = 'files') {
+	function button($label 'button', $group = null, $div_id = 'cakeupload-uploader' $folder = 'files') {
 		echo $this->Html->css('/cakeupload/css/fileuploader.css', null, array('inline' => false));
 		echo $this->Html->script('/cakeupload/js/fileuploader.js', array('inline' => false, 'once' => true));
-		echo "<div id='cakeupload-uploader'>";
+		echo "<div id='".$div_id."'>";
 		echo "<noscript>";
 		echo "<p>Please enable JavaScript to use file uploader.</p>";
 		echo "<!-- or put a simple form for upload here -->";
 		echo "</noscript>";
 		echo "</div>";
+		$id = uniqid();
 		ob_start();
 		?>
 
-        function createUploader(){            
-            var uploader = new qq.FileUploader({
-                element: document.getElementById('cakeupload-uploader'),
-                action: '<?php echo $this->Html->url(array('plugin' => 'cakeupload', 'controller' => 'cakeupload_files', 'action' => 'upload')); ?>',
-                params: { group: '<?php echo $group; ?>'},
-                debug: true
-            });           
-        }
-        
-        // in your app create uploader as soon as the DOM is ready
-        // don't wait for the window to load  
-        window.onload = createUploader;     
+		function addOnloadEvent(fnc){
+		  if ( typeof window.addEventListener != "undefined" )
+		    window.addEventListener( "load", fnc, false );
+		  else if ( typeof window.attachEvent != "undefined" ) {
+		    window.attachEvent( "onload", fnc );
+		  }
+		  else {
+		    if ( window.onload != null ) {
+		      var oldOnload = window.onload;
+		      window.onload = function ( e ) {
+		        oldOnload( e );
+		        window[fnc]();
+	 	      };
+		    }
+		    else
+		      window.onload = fnc;
+		    }
+		}
+	
+	        function createUploader<?php echo $id ?>(){            
+	            var uploader = new qq.FileUploader({
+	                element: document.getElementById('<?php echo $div_id; ?>'),
+	                action: '<?php echo $this->Html->url(array('plugin' => 'cakeupload', 'controller' => 'cakeupload_files', 'action' => 'upload')); ?>',
+	                params: { group: '<?php echo $group; ?>'},
+	                debug: true
+	            }, '<?php echo $label; ?>');           
+	        }
+	        
+	        // in your app create uploader as soon as the DOM is ready
+	        // don't wait for the window to load  
+	        addOnloadEvent(createUploader<?php echo $id; ?>);    
 
 
 		<?php
